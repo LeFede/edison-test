b54fd4a7a18de5158fb012c8fba15b325d71cf05
LeFede <lefedeok@gmail.com>
2024-12-07 19:04:08 -0300

    go

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // astro.config.mjs
 import { defineConfig } from "astro/config";
 import awsAmplify from "astro-aws-amplify";
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+// import react from "@astrojs/react";
+// import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  integrations: [react(), tailwind()],
-  output: "server", // output: 'hybrid'
+  // integrations: [react(), tailwind()],
+  output: "static", // output: 'hybrid'
   adapter: awsAmplify(),
 });
